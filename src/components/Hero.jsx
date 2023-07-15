88a98a4bad40abe94cd9fd048e6cf7db33218acd
EdwinSch <edwinschutjes@gmail.com>
2023-07-15 17:02:39 +0200

    fix: styling issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,16 @@ import Button from "./Button";
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="hero-content">
-        <h1>
-          Maximize skill,
-          <br />
-          minimize budget
-        </h1>
-        <p className="text">
-          Our modern courses across a range of in-demand skills will give you
-          the knowledge you need to live the life you want.
-        </p>
-        <Button color={"linear-gradient(0deg, #f02aa6, #ff6f48)"} />
-      </div>
-      <div className="hero-image">
-        <figure>{/* CSS dynamic */}</figure>
-      </div>
+      <h1>
+        Maximize skill,
+        <br />
+        minimize budget
+      </h1>
+      <p className="text">
+        Our modern courses across a range of in-demand skills will give you the
+        knowledge you need to live the life you want.
+      </p>
+      <Button color={"linear-gradient(0deg, #f02aa6, #ff6f48)"} />
     </section>
   );
 };

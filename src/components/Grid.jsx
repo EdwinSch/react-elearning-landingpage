6cc8db6392394ddf8c6ddde9355fea3c31a1e830
EdwinSch <edwinschutjes@gmail.com>
2023-07-15 15:09:18 +0200

    style: tiles

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,6 +4,9 @@ import Tile from "./Tile";
 const Grid = () => {
   return (
     <section className="grid-container">
+      <article className="tile first-tile">
+        Check out our most popular courses!
+      </article>
       {usps.map((usp) => {
         return <Tile key={usp.id} {...usp} />;
       })}
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,8 +1,10 @@
 const Tile = ({ image, title, text }) => {
   return (
     <article className="tile">
+      <img src={image} alt={title} />
       <h2>{title}</h2>
       <p>{text}</p>
+      <p className="link">get started</p>
     </article>
   );
 };
